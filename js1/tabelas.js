54609135ba929b837da6202e12d3a1dd2a2adb2d
hilton <hilton@lepidus.com.br>
2020-10-20 17:07:59 -0400

    Primeiro exercicio de javaScript: Tabuada multiplicação
    Signed-off-by: hilton <hilton@lepidus.com.br>

---
+++ js1/tabelas.js
@@ -0,0 +1,18 @@
+
+
+function geraTabela(){
+	tabelaFinal = "";
+
+	for (var i = 1; i <= 10; i++) {
+		cabecalho = "<thead><tr><th colspan=2> Produtos de "+i+" </th></tr></thead>";
+		for (var j = 1; j<=10; j++){
+			celulas = "<tr> <td> "+i+'x'+j+" </td> <td> "+i*j+" </td> </tr>";
+			tabelaFinal+=celulas;
+		}
+		tabelaFinal = "<table border="+1+"> "+cabecalho+""+tabelaFinal+"</table>";
+		document.writeln(tabelaFinal);
+		tabelaFinal = "";
+	}
+}
+
+geraTabela();
