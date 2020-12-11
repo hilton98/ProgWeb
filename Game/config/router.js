b1e0d79b3e2c474c3ab7e494047cd598b1cbadfc
Hilton dos Santos Costa Neto <hilton@lepidus.com.br>
2020-12-11 10:12:55 -0400

    Game: Desenvolvimento - Criação base de dados e tabelas
    Signed-off-by: hilton98<hscn@icomp.ufam.edu.br>

--- Game/config/router.js
+++ Game/config/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mainController = require("../app/controllers/main");
+const areaController = require("../app/controllers/area");
 const router = express.Router();
 
 router.get("/sobre", mainController.sobre);
 router.get("/ui", mainController.ui);
+router.get("/area", areaController.index);
 router.get("/game", mainController.game);
 router.get("/", mainController.index);
 
